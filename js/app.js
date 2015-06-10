009bcd847c18134d0b124336100c0c78e8fb8b79
jordan23jy <jordanyong23@gmail.com>
2015-06-10 16:06:12 +0800

    initial commit

---
+++ js/app.js
@@ -0,0 +1,8 @@
+// Foundation JavaScript
+// Documentation can be found at: http://foundation.zurb.com/docs
+$(document).foundation();
+
+//TODO:
+// create matrix
+// include random numbers
+// merge numbers
