0ba4a73e17127678ac6efe3061324ed06e9a4dab
jordan23jy <jordanyong23@gmail.com>
2015-06-10 17:57:25 +0800

    modified grid render

--- js/grids.js
+++ js/grids.js
@@ -9,7 +9,6 @@ Grid.prototype.init = function() {
 	var self = this;
 	var cell;
 	for (var i = 0; i < self.noGrids * self.noGrids; i++) {
-		cell =
 		self.gridArray[i] = new Cells(i, self.noGrids);
 	}
 	console.log(self.gridArray);
@@ -17,22 +16,45 @@ Grid.prototype.init = function() {
 
 Grid.prototype.render = function() {
 	var self = this;
-	var elemRow = "<div class='grid-row'></div>";
-	for (var i = 0; i < self.noGrids; i++) {
-		$('#grids').append(elemRow);
-	}
-	var elemCell = "<div class='grid-cell'></div>";
-	for (var j = 0; j < self.noGrids; j++) {
-		$('.grid-row').append(elemCell);
-	}
+	var cellArray = [];
+	var parent = document.getElementById('grids');
 
+	for (var i = 0; i < self.noGrids; i++) {
+		var row = document.createElement('div');
+		row.className = 'grid-row';
+		parent.appendChild(row);
+
+		for (var j = 0; j < self.noGrids; j++) {
+			var cols = document.createElement('div');
+			cols.className = 'grid-cell';
+			row.appendChild(cols);
+			cellArray.push(cols);
+		}
+	}
+	return cellArray;
+};
+
+Grid.prototype.displayValue = function() {
 
 }
+
+Grid.prototype.randomValue = function(num1, num2) {
+	return  Math.random() < 0.5 ? num1 : num2;
+};
+
+Grid.prototype.randomIndex = function() {
+	return Math.floor((Math.random() * this.noGrids * this.noGrids) + 1);
+};
+
+Grid.prototype.setCellValue = function(index, value) {
+	this.gridArray[index] = value;
+
+};
 
 var grids = new Grid(NO_GRIDS);
 
 grids.init();
-grids.render();
+var m = grids.render();
 
 
 /*========== CELLS ==========*/
@@ -40,9 +62,8 @@ function Cells(index, noGrids) {
 	this.index = index;
 	this.value = 0;
 	this.x = Math.floor(index / noGrids);
+	this.noGrids = noGrids;
 	this.y = index % noGrids;
 }
 
-Cells.prototype.test1 = function() {
-	console.log("hello");
-};
+
