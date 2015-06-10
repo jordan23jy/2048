51e67dd23924e1f78ec3fe333a17530edc0e1497
jordan23jy <jordanyong23@gmail.com>
2015-06-10 20:38:55 +0800

    add random values to cell

--- js/grids.js
+++ js/grids.js
@@ -1,22 +1,22 @@
 var NO_GRIDS = 4;
 
-function Grid(noGrids) {
+var Grid = function(noGrids) {
 	this.noGrids = NO_GRIDS;
-	this.gridArray = [];
+	this.cellArray = []
 }
 
 Grid.prototype.init = function() {
 	var self = this;
 	var cell;
-	for (var i = 0; i < self.noGrids * self.noGrids; i++) {
-		self.gridArray[i] = new Cells(i, self.noGrids);
-	}
-	console.log(self.gridArray);
+	// render grid elements
+	this.render();
+	// set two random values in cells
+	self.randomValue();
+	self.randomValue();
 };
 
 Grid.prototype.render = function() {
 	var self = this;
-	var cellArray = [];
 	var parent = document.getElementById('grids');
 
 	for (var i = 0; i < self.noGrids; i++) {
@@ -28,42 +28,48 @@ Grid.prototype.render = function() {
 			var cols = document.createElement('div');
 			cols.className = 'grid-cell';
 			row.appendChild(cols);
-			cellArray.push(cols);
+			self.cellArray.push(new Cells(i, self.noGrids, cols));
 		}
 	}
-	return cellArray;
+	return self.cellArray;
 };
 
-Grid.prototype.displayValue = function() {
+Grid.prototype.randomValue = function() {
+	var self = this;
+	var randomValue = Math.random() < 0.5 ? 2 : 4;
+	var randomIndex = Math.floor((Math.random() * this.noGrids * this.noGrids));
 
-}
-
-Grid.prototype.randomValue = function(num1, num2) {
-	return  Math.random() < 0.5 ? num1 : num2;
-};
-
-Grid.prototype.randomIndex = function() {
-	return Math.floor((Math.random() * this.noGrids * this.noGrids) + 1);
+	// generate random index only on available cells
+	// otherwise recurse function
+	if (self.cellArray[randomIndex].value === 0) {
+		console.log("passed: " + randomIndex);
+		self.setCellValue(randomIndex, randomValue);
+	} else {
+		console.log('recursion');
+		self.randomValue();
+	}
 };
 
 Grid.prototype.setCellValue = function(index, value) {
-	this.gridArray[index] = value;
+	var cell = this.cellArray[index];
 
+	cell.elem.innerHTML = value;
+	cell.value = value;
 };
 
 var grids = new Grid(NO_GRIDS);
-
 grids.init();
-var m = grids.render();
+
 
 
 /*========== CELLS ==========*/
-function Cells(index, noGrids) {
+function Cells(index, noGrids, elem) {
 	this.index = index;
 	this.value = 0;
 	this.x = Math.floor(index / noGrids);
 	this.noGrids = noGrids;
 	this.y = index % noGrids;
+	this.elem = elem;
 }
 
 
